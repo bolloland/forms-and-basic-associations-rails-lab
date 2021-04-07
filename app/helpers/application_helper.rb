d29fa83a5d1692e97f40244ceb6c16855df7c8e1
Ryan Bollettino <ryan.bollettino@gmail.com>
2021-04-07 13:18:10 -0500

    commit tree dc6526db24190500fdab622e2cdb32a9a9c48a4e

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+    def genre_name=(name)
+        self.genre = Genre.find_or_create_by(name: name)
+      end
+    
+      def genre_name
+         self.genre ? self.genre.name : nil
+      end
 end
